2b96ce140a72dd46afc8e9e57c0ce3b21c90bcf1
Stefan Harabagiu <stefan.harabagiu@gmail.com>
2019-04-25 22:15:49 +0200

    Solved navigation to and from AddPlan activity

--- app/src/main/java/com/example/dailyassistant/AddPlan.java
+++ app/src/main/java/com/example/dailyassistant/AddPlan.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 
 public class AddPlan extends AppCompatActivity {
@@ -15,6 +16,13 @@ public class AddPlan extends AppCompatActivity {
 
         Toolbar childToolbar = (Toolbar) findViewById(R.id.child_toolbar);
         setSupportActionBar(childToolbar);
+
+        childToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         ActionBar ab =getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);

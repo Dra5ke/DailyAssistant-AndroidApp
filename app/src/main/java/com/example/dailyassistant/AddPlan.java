2dc11c6866c68d32eb9e031baa7421461b9544de
Stefan Harabagiu <stefan.harabagiu@gmail.com>
2019-04-25 22:11:27 +0200

    AppBar to return in AddPlan activity

--- app/src/main/java/com/example/dailyassistant/AddPlan.java
+++ app/src/main/java/com/example/dailyassistant/AddPlan.java
@@ -1,7 +1,10 @@
 package com.example.dailyassistant;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class AddPlan extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class AddPlan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plan);
+
+        Toolbar childToolbar = (Toolbar) findViewById(R.id.child_toolbar);
+        setSupportActionBar(childToolbar);
+
+        ActionBar ab =getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/dailyassistant/MainActivity.java
+++ app/src/main/java/com/example/dailyassistant/MainActivity.java
@@ -1,23 +1,17 @@
 package com.example.dailyassistant;
 
 import android.app.DatePickerDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 
 import com.google.firebase.auth.FirebaseAuth;

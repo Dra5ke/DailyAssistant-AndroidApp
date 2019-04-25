60293c62d225bdde44f2d5ee28889ef1a7588e0e
Stefan Harabagiu <stefan.harabagiu@gmail.com>
2019-04-25 11:57:36 +0200

    ViewHolder class finished

--- app/src/main/java/com/example/dailyassistant/PlanAdapter.java
+++ app/src/main/java/com/example/dailyassistant/PlanAdapter.java
@@ -3,6 +3,8 @@ package com.example.dailyassistant;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.ViewHolder> {
 
@@ -18,8 +20,17 @@ public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.ViewHolder> {
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView Title;
+        TextView Description;
+        Button editDate;
+
         ViewHolder(View itemView) {
             super(itemView);
+
+            Title = itemView.findViewById(R.id.tv_title);
+            Description = itemView.findViewById(R.id.tv_description);
+            editDate = itemView.findViewById(R.id.tv_editDate);
         }
     }
 }
